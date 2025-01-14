58c969f531e1b40828f42a53efb8266a8aa20e6b
AdrianZayas <a01572535@tec.mx>
2025-01-13 19:21:03 -0600

    intake

---
+++ src/main/Subsystems/Intake/Intake.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Intake.h"
+
+Intake::Intake() = default;
+
+void Intake::setVoltage(units::volt_t voltage){
+    intakeMotor.SetVoltage(voltage);
+}
+
+double Intake::getVoltage(){
+    return intakeMotor.GetMotorVoltage().GetValueAsDouble();
+}
+
+
+frc2::CommandPtr Intake::moveIntake(units::volt_t voltage){return this->RunOnce([this, voltage] {this->setVoltage(voltage);});};
+
+
+// This method will be called once per scheduler run
+void Intake::Periodic() {
+  frc::SmartDashboard::PutBoolean("INTAKE ACTIVATED?", getVoltage() > 0.0);
+}

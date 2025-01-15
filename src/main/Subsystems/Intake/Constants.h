076b54f9a38b304f91fbc58846f4dfda55c1d1bd
adrianzayas0 <143229114+adrianzayas0@users.noreply.github.com>
2025-01-14 21:31:50 -0600

    Intake (#3)
    * intake
    
    * intake subsystem
    
    * telemetria y intake Config con mayuscula

---
+++ src/main/Subsystems/Intake/Constants.h
@@ -0,0 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+//.
+
+#pragma once
+#include <units/voltage.h>
+#include "Subsystems/Intake/Intake.h"
+
+struct Constants {
+ public:
+
+  constexpr static const units::volt_t GroundGrabVolts = 6.0_V;
+  constexpr static const units::volt_t stopVolts = 0.0_V;
+  constexpr static const units::volt_t reverseVolts = -6.0_V;
+
+constexpr static const OverTalonFXConfig IntakeConfig(){
+  OverTalonFXConfig intakeConfig;
+  intakeConfig.MotorId = 1;
+  intakeConfig.NeutralMode = ControllerNeutralMode::Brake;
+
+  return intakeConfig;
+}
+
+};
---
+++ src/main/Subsystems/Intake/Intake.h
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+//.
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include "OvertureLib/MotorControllers/OverTalonFX/OverTalonFX.h"
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/Command.h>
+#include <frc2/command/CommandHelper.h>
+#include "Constants.h"
+#include <frc/smartdashboard/SmartDashboard.h>
+#include "Subsystems/Intake/Constants.h"
+
+
+class Intake : public frc2::SubsystemBase {
+ public:
+  Intake();
+
+  void setVoltage(units::volt_t voltage);
+  double getVoltage();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+  frc2::CommandPtr moveIntake(units::volt_t voltage);
+
+  private:
+ //ID 20
+    OverTalonFX intakeMotor{Constants::IntakeConfig() , "rio"};
+};

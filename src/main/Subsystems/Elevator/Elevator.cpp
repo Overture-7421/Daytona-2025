ceddf07e55d46763f6299d6926637b23bcb772de
VanessaCarranza <138523212+VanessaCarranza@users.noreply.github.com>
2025-01-15 17:23:46 -0600

    Elevator Subsystemn (#4)
    * Elevator Subsystemn
    
    * Elevator Subsystem updated
    
    * elevator subsystem fixed yay
    
    * elevator subsystem fixed and if statement and publishing telemetry
    
    * Elevator Subsystem build fixed:)

---
+++ src/main/Subsystems/Elevator/Elevator.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+#include "Elevator.h"
+
+Elevator::Elevator(){
+    rightElevatorMotor.setFollow(leftElevatorMotor.GetDeviceID(), true);
+
+    leftElevatorMotor.setSensorToMechanism(ElevatorConstants::LowerSensorToMechanism);
+
+}
+
+void Elevator::setPosition(units::meter_t position){
+
+    units::turn_t positionInRotations{position.value() / (ElevatorConstants::Diameter.value() * M_PI)};
+    leftElevatorMotor.SetControl(elevatorVoltage.WithPosition(positionInRotations).WithEnableFOC(true));
+    
+};
+
+bool Elevator::isElevatorAtPosition(units::meter_t elevatorPosition){
+    units::meter_t elevatorError = elevatorPosition - units::meter_t(leftElevatorMotor.GetPosition().GetValueAsDouble()* (ElevatorConstants::Diameter.value() * 3.14159265358979323846));
+    return (units::math::abs(elevatorError) < 0.015_m);
+      
+}
+
+frc2::CommandPtr Elevator::setElevatorCommand(units::meter_t elevatorPosition){
+    return frc2::FunctionalCommand(
+        [&]() {setPosition(elevatorPosition);},
+        [&]() {},
+        [&](bool interrupted) {},
+        [&]() {return isElevatorAtPosition(elevatorPosition);},
+        { this }
+    ).ToPtr();
+}
+// This method will be called once per scheduler run
+void Elevator::Periodic() {
+
+    //no sé si está bien jiji
+        double currentPosition = {leftElevatorMotor.GetPosition().GetValueAsDouble() * (ElevatorConstants::Diameter.value() * 3.14159265358979323846)};
+    frc::SmartDashboard::PutNumber("Elevator/CurrentPosition", currentPosition);
+
+}
---
+++ src/main/Subsystems/Elevator/Elevator.h
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/SubsystemBase.h>
+#include "OvertureLib/MotorControllers/OverTalonFX/OverTalonFX.h"
+#include <Subsystems/Elevator/Constants.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <units/math.h>
+#include <frc2/command/FunctionalCommand.h>
+
+class Elevator : public frc2::SubsystemBase {
+ public:
+  Elevator();
+
+  /**
+   * Will be called periodically whenever the CommandScheduler runs.
+   */
+  void Periodic() override;
+
+  void setPosition(units::meter_t position);
+
+  void getCurrentPosition();
+
+  bool isElevatorAtPosition(units::meter_t elevatorPosition);
+  frc2::CommandPtr setElevatorCommand(units::meter_t elevatorPosition);
+
+ private:
+  OverTalonFX leftElevatorMotor{ElevatorConstants::LeftConfig(), "rio"};
+  OverTalonFX rightElevatorMotor{ElevatorConstants::RightConfig(),"rio"};
+
+  MotionMagicVoltage elevatorVoltage{0_tr};
+
+
+
+  // Components (e.g. motor controllers and sensors) should generally be
+  // declared private and exposed only through public methods.
+};

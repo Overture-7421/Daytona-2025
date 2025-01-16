b728484ac0210474280ddd54f8b08ce04e45caa5
Xx_GaboReyna_xX <143459578+Drfeelgood999@users.noreply.github.com>
2025-01-15 23:57:28 -0600

    HIgh Algae (#25)
    * High Algae command ready fpor revision (same exact copy of low algae but with other constants) ALSO I changed the constructors of both commands for them not to use the OverXbox COntroller since i realized it had no use
    
    * Changed the logic in both High and Low algae commands so that is just one command being executed after another, since there was a parallel command before, with no use.

--- src/main/Commands/LowAlgae/LowAlgae.cpp
+++ src/main/Commands/LowAlgae/LowAlgae.cpp
@@ -6,12 +6,12 @@
 #include "Subsystems/Arm/Constants.h"
 #include "Subsystems/Elevator/Constants.h"
 
-frc2::CommandPtr LowAlgae(Arm *arm, Elevator *elevator, OverXboxController *overxboxController) {
+frc2::CommandPtr LowAlgae(Arm *arm, Elevator *elevator) {
     return frc2::cmd::Sequence(elevator->setElevatorCommand(ElevatorConstants::BottomAlgaePosition),
             frc2::cmd::WaitUntil([elevator] {
                 return elevator->isElevatorAtPosition(ElevatorConstants::BottomAlgaePosition);
             }),
-            frc2::cmd::Parallel(arm->setArmCommand(Constants::ArmL2Reef, Constants::WristL2Reef)),
+            arm->setArmCommand(Constants::ArmL2Reef, Constants::WristL2Reef),
 
             frc2::cmd::WaitUntil([arm] {
                 return arm->isArmAtPosition(Constants::ArmL2Reef, Constants::WristL2Reef);

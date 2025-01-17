365d592011f10f6cd46b653d77f5e0078b3aac3a
GabrielReynaFranco <a01572655@tec.mx>
2025-01-16 20:15:17 -0600

    Closed Command done, please review.

---
+++ src/main/Commands/ClosedCommand/ClosedCommand.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "ClosedCommand.h"
+
+frc2::CommandPtr ClosedCommand(Arm *arm, Elevator *elevator) {
+    return frc2::cmd::Sequence(arm->setArmCommand(Constants::ArmClosed, Constants::WristClosed),
+            frc2::cmd::WaitUntil([arm] {
+                return arm->isArmAtPosition(Constants::ArmClosed, Constants::WristClosed);
+            }),
+            elevator->setElevatorCommand(ElevatorConstants::ClosedPosition), frc2::cmd::WaitUntil([elevator] {
+                return elevator->isElevatorAtPosition(ElevatorConstants::ClosedPosition);
+
+            })
+
+    );
+}

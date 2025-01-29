0cc14b72a657cd12af9ae67ebae7b67e922b89d2
GabrielReynaFranco <a01572655@tec.mx>
2025-01-28 19:59:08 -0600

    Bane?

--- src/main/Commands/HighAlgae/HighAlgae.cpp
+++ src/main/Commands/HighAlgae/HighAlgae.cpp
@@ -3,13 +3,12 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "HighAlgae.h"
-#include "Commands/AlignToPose/AlignToPose.h"
 #include "Commands/ArmMotion/ArmMotion.h"
 
-frc::Pose2d pose {3.644_m, 2.976_m, 150.0_deg};
 
-frc2::CommandPtr HighAlgae(Arm *arm, Elevator *elevator, Chassis *chassis) {
-    return frc2::cmd::Parallel(AlignToPose(chassis, pose).ToPtr(),
+
+frc2::CommandPtr HighAlgae(Arm *arm, Elevator *elevator) {
+    return frc2::cmd::Parallel(
             elevator->setElevatorCommand(ElevatorConstants::HighAlgae),
             ArmMotion(elevator, arm, ArmConstants::ArmHighAlgae, ArmConstants::WristHighAlgae,
                     ElevatorConstants::HighAlgae).ToPtr());

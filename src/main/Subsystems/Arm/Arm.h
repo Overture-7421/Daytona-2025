ff9e5df210fb941a09af8ac879e8fad3aa3bb106
Vanessa_Carranza <v.carranza.carri@gmail.com>
2025-08-12 15:54:47 -0600

    Arm Subsystem and Constants ALMOST ready

--- src/main/Subsystems/Arm/Arm.h
+++ src/main/Subsystems/Arm/Arm.h
@@ -3,7 +3,7 @@
 #include <frc2/command/SubsystemBase.h>
 #include <frc2/command/CommandPtr.h>
 #include <frc/smartdashboard/SmartDashboard.h>
-#include <frc/controller/ArmFeedforward.h>
+#include <frc/MathUtil.h>
 #include <frc2/command/FunctionalCommand.h>
 #include <OvertureLib/Utils/Logging/Logging.h>
 
@@ -13,8 +13,16 @@ class Arm: public frc2::SubsystemBase {
 public:
     Arm();
 
+    void setToAngle(units::degree_t armAngle);
+    bool isArmAtPosition(units::degree_t armAngle);
+    units::degree_t getCurrentAngle();
+    frc2::CommandPtr setArmCommand(units::degree_t armAngle);
+
     void Periodic() override;
 
 private:
 
+    VoltageOut armVoltage {0_V};
+    OverTalonFX armMotor {ArmConstants::ArmConfig(), "rio"};
+    OverCANCoder armCANCoder {ArmConstants::ArmCANConfig(), "rio"};
 };
--- src/main/Subsystems/Arm/ArmConstants.h
+++ src/main/Subsystems/Arm/ArmConstants.h
@@ -8,25 +8,51 @@
 
 struct ArmConstants {
 
+    constexpr static const units::degree_t ArmRangeError = 0.0_deg;
+
+    constexpr static const units::degree_t ArmScoreL1 = 0_deg;
+
+    constexpr static const units::degree_t FrontArmScoreL2 = 0_deg;
+    constexpr static const units::degree_t FrontArmScoreL3 = 0_deg;
+    constexpr static const units::degree_t FrontArmScoreL4 = 0_deg;
+
+    constexpr static const units::degree_t BackArmScoreL2 = 0_deg;
+    constexpr static const units::degree_t BackArmScoreL3 = 0_deg;
+    constexpr static const units::degree_t BackArmScoreL4 = 0_deg;
+
+    constexpr static const units::degree_t AutonomousCoralArm = 0_deg;
+
+    constexpr static const units::degree_t ArmSustainPosition = 0_deg;
+    constexpr static const units::degree_t ArmClosedPosition = 0_deg;
+
+    constexpr static const units::degree_t ArmAlgaePosition = 0_deg;
+    constexpr static const units::degree_t ArmNetPosition = 0_deg;
+    constexpr static const units::degree_t GroundAlgae = 0_deg;
+
+    constexpr static const units::turns_per_second_t ArmCruiseVelocity = 0_tps;
+    constexpr static const units::turns_per_second_squared_t ArmCruiseAcceleration = 0_tr_per_s_sq;
+
+    constexpr static const double ArmRotorToSensor = 0;
+
     constexpr static const double ArmMotorId = 23;
     constexpr static const double ArmCANCoderId = 25;
 
-    constexpr static const OverTalonFXConfig ArmLeftConfig() {
-        OverTalonFXConfig armLeftConfig;
-        armLeftConfig.MotorId = ArmMotorId;
-        armLeftConfig.NeutralMode = ControllerNeutralMode::Brake;
-        armLeftConfig.useFOC = true;
-        armLeftConfig.Inverted = true;
+    constexpr static const OverTalonFXConfig ArmConfig() { //LImites cuestionables
+        OverTalonFXConfig armConfig;
+        armConfig.MotorId = ArmMotorId;
+        armConfig.NeutralMode = ControllerNeutralMode::Brake;
+        armConfig.useFOC = true;
+        armConfig.Inverted = true;
 
-        armLeftConfig.ClosedLoopRampRate = 0.05_s;
-        armLeftConfig.CurrentLimit = 30_A;
-        armLeftConfig.StatorCurrentLimit = 120_A;
-        armLeftConfig.TriggerThreshold = 40_A;
-        armLeftConfig.TriggerThresholdTime = 0.5_s;
-        armLeftConfig.PIDConfigs.GravityType = 1;
-        armLeftConfig.PIDConfigs.WithKG(0.0).WithKV(0.0).WithKP(0.0);
+        armConfig.ClosedLoopRampRate = 0.05_s;
+        armConfig.CurrentLimit = 30_A;
+        armConfig.StatorCurrentLimit = 120_A;
+        armConfig.TriggerThreshold = 40_A;
+        armConfig.TriggerThresholdTime = 0.5_s;
+        armConfig.PIDConfigs.GravityType = 1;
+        armConfig.PIDConfigs.WithKG(0.0).WithKV(0.0).WithKP(0.0);
 
-        return armLeftConfig;
+        return armConfig;
     }
 
     constexpr static const CanCoderConfig ArmCANConfig() {

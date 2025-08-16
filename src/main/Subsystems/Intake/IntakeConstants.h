bf25ec9f1b54c748e17b2f08b30938f9fb81f81c
Luis Leyva <luisoleyvan@gmail.com>
2025-08-15 19:00:52 -0600

    si

--- src/main/Subsystems/Intake/IntakeConstants.h
+++ src/main/Subsystems/Intake/IntakeConstants.h
@@ -9,113 +9,119 @@
 #include "Enums/Heading.h"
 #include "Enums/Positions.h"
 
-struct IntakeConstants {
+struct IntakeValues {
 
-    inline static const std::map<Positions, units::volt_t, units::volt_t, units::degree_t > IntakePositions = {
+	units::volt_t rollers;
+	units::volt_t intake;
+	units::degree_t pivot;
 
-        /*The intake subsystem consists in three motors running at the same time, hence three
-        variables will be needed, the first volt refers to the centering motor, the secon to the
-        rollers and finally the degree type variable refers to the pivot*/
+}
 
-        {Positions::AlgaeGround, 0.0_V,0.0_V,0_deg}, 
-        {Positions::AlgaeHold, 0.0_V,0.0_V,0_deg},
-        {Positions::AlgaeHighReef,0.0_V,0.0_V,0_deg},
-        {Positions::AlgaeLowReef, 0.0_V,0.0_V,0_deg},
-        {Positions::CoralandAlgae, 0.0_V,0.0_V,30_deg}, //To be defined
-        {Positions::CoralHold, 0.0_V,0.0_V,0_deg}, //To be defined
-        {Positions::InitialPosition, 0.0_V,0.0_V,0_deg},
-        {Positions::Intake, 3.0_V,4.0_V,80_deg}, //To be defined
-        {Positions::IntakeCoralStation, 3.0_V,4.0_V,10_deg},//To be defined
-        {Positions::L1Confirm, -2.0_V,0.0_V,30_deg}, //To be defined
-        {Positions::L1Position, 0.0_V,0.0_V,30_deg}, //To be defined
-        {Positions::L2Back, 0.0_V,0.0_V,0_deg},
-        {Positions::L2BackConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::L2Front, 0.0_V,0.0_V,0_deg},
-        {Positions::L2FrontConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::L3Back, 0.0_V,0.0_V,0_deg},
-        {Positions::L3BackConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::L3Front, 0.0_V,0.0_V,0_deg},
-        {Positions::L3FrontConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::L4Back, 0.0_V,0.0_V,0_deg},
-        {Positions::L4BackConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::L4Front, 0.0_V,0.0_V,0_deg},
-        {Positions::L4FrontConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::NetPosition,0.0_V,0.0_V,0_deg},
-        {Positions::NetConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::ProcessorPosition, 0.0_V,0.0_V,0_deg},
-        {Positions::ProcessorConfirm, 0.0_V,0.0_V,0_deg},
-        {Positions::SustainedPosition, 0.0_V,0.0_V,0_deg}        
-    };
+inline static const std::map<Positions, IntakeValues > IntakePositions = {
 
-    constexpr static const units::degree_t IntakeRangeError = 1_deg;
+	/*The intake subsystem consists in three motors running at the same time, hence three
+	variables will be needed, the first volt refers to the centering motor, the secon to the
+	rollers and finally the degree type variable refers to the pivot*/
 
-    constexpr static const units::turns_per_second_t IntakeCruiseVelocity = 0_tps;
-    constexpr static const units::turns_per_second_squared_t IntakeCruiseAcceleration = 0_tr_per_s_sq;
+	{Positions::AlgaeGround, 0.0_V,0.0_V,0_deg},
+	{Positions::AlgaeHold, 0.0_V,0.0_V,0_deg},
+	{Positions::AlgaeHighReef,0.0_V,0.0_V,0_deg},
+	{Positions::AlgaeLowReef, 0.0_V,0.0_V,0_deg},
+	{Positions::CoralandAlgae, 0.0_V,0.0_V,30_deg}, //To be defined
+	{Positions::CoralHold, 0.0_V,0.0_V,0_deg}, //To be defined
+	{Positions::InitialPosition, 0.0_V,0.0_V,0_deg},
+	{Positions::Intake, 3.0_V,4.0_V,80_deg}, //To be defined
+	{Positions::IntakeCoralStation, 3.0_V,4.0_V,10_deg},//To be defined
+	{Positions::L1Confirm, -2.0_V,0.0_V,30_deg}, //To be defined
+	{Positions::L1Position, 0.0_V,0.0_V,30_deg}, //To be defined
+	{Positions::L2Back, 0.0_V,0.0_V,0_deg},
+	{Positions::L2BackConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::L2Front, 0.0_V,0.0_V,0_deg},
+	{Positions::L2FrontConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::L3Back, 0.0_V,0.0_V,0_deg},
+	{Positions::L3BackConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::L3Front, 0.0_V,0.0_V,0_deg},
+	{Positions::L3FrontConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::L4Back, 0.0_V,0.0_V,0_deg},
+	{Positions::L4BackConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::L4Front, 0.0_V,0.0_V,0_deg},
+	{Positions::L4FrontConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::NetPosition,0.0_V,0.0_V,0_deg},
+	{Positions::NetConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::ProcessorPosition, 0.0_V,0.0_V,0_deg},
+	{Positions::ProcessorConfirm, 0.0_V,0.0_V,0_deg},
+	{Positions::SustainedPosition, 0.0_V,0.0_V,0_deg}
+};
 
-    constexpr static const double IntakeRotorToSensor = 0;
+constexpr static const units::degree_t IntakeRangeError = 1_deg;
 
-    constexpr static const double IntakeMotorId = 26;
-    constexpr static const double IntakeCANCoderId = 27;
+constexpr static const units::turns_per_second_t IntakeCruiseVelocity = 0_tps;
+constexpr static const units::turns_per_second_squared_t IntakeCruiseAcceleration = 0_tr_per_s_sq;
 
-    constexpr static const double RollersMotorId = 29;
-    constexpr static const double CenteringMotorId = 31;
+constexpr static const double IntakeRotorToSensor = 0;
 
-    constexpr static const OverTalonFXConfig IntakeConfig() { //Limites cuestionables
-        OverTalonFXConfig intakeConfig;
-        intakeConfig.MotorId = IntakeMotorId;
-        intakeConfig.NeutralMode = ControllerNeutralMode::Brake;
-        intakeConfig.useFOC = true;
-        intakeConfig.Inverted = true;
+constexpr static const double IntakeMotorId = 26;
+constexpr static const double IntakeCANCoderId = 27;
 
-        intakeConfig.ClosedLoopRampRate = 0.05_s;
-        intakeConfig.CurrentLimit = 30_A;
-        intakeConfig.StatorCurrentLimit = 120_A;
-        intakeConfig.TriggerThreshold = 40_A;
-        intakeConfig.TriggerThresholdTime = 0.5_s;
-        intakeConfig.PIDConfigs.GravityType = 1;
-        intakeConfig.PIDConfigs.WithKG(0.0).WithKV(0.0).WithKP(0.0);
+constexpr static const double RollersMotorId = 29;
+constexpr static const double CenteringMotorId = 31;
 
-        return intakeConfig;
-    }
+constexpr static const OverTalonFXConfig IntakeConfig() { //Limites cuestionables
+	OverTalonFXConfig intakeConfig;
+	intakeConfig.MotorId = IntakeMotorId;
+	intakeConfig.NeutralMode = ControllerNeutralMode::Brake;
+	intakeConfig.useFOC = true;
+	intakeConfig.Inverted = true;
 
-    constexpr static const CanCoderConfig IntakeCANConfig() {
-        CanCoderConfig intakeCANConfig;
-        intakeCANConfig.CanCoderId = IntakeCANCoderId;
-        intakeCANConfig.Offset = 0.0_tr;
+	intakeConfig.ClosedLoopRampRate = 0.05_s;
+	intakeConfig.CurrentLimit = 30_A;
+	intakeConfig.StatorCurrentLimit = 120_A;
+	intakeConfig.TriggerThreshold = 40_A;
+	intakeConfig.TriggerThresholdTime = 0.5_s;
+	intakeConfig.PIDConfigs.GravityType = 1;
+	intakeConfig.PIDConfigs.WithKG(0.0).WithKV(0.0).WithKP(0.0);
 
-        return intakeCANConfig;
-    }
+	return intakeConfig;
+}
 
-    constexpr static const OverTalonFXConfig RollersConfig() { //Limites cuestionables
-        OverTalonFXConfig rollersConfig;
-        rollersConfig.MotorId = RollersMotorId;
-        rollersConfig.NeutralMode = ControllerNeutralMode::Brake;
-        rollersConfig.Inverted = true;
+constexpr static const CanCoderConfig IntakeCANConfig() {
+	CanCoderConfig intakeCANConfig;
+	intakeCANConfig.CanCoderId = IntakeCANCoderId;
+	intakeCANConfig.Offset = 0.0_tr;
 
-        rollersConfig.CurrentLimit = 25_A;
-        rollersConfig.StatorCurrentLimit = 120_A;
-        rollersConfig.TriggerThreshold = 40_A;
-        rollersConfig.TriggerThresholdTime = 0.5_s;
-        rollersConfig.ClosedLoopRampRate = 0.0_s;
-        rollersConfig.OpenLoopRampRate = 0.05_s;
+	return intakeCANConfig;
+}
 
-        return rollersConfig;
-    }
+constexpr static const OverTalonFXConfig RollersConfig() { //Limites cuestionables
+	OverTalonFXConfig rollersConfig;
+	rollersConfig.MotorId = RollersMotorId;
+	rollersConfig.NeutralMode = ControllerNeutralMode::Brake;
+	rollersConfig.Inverted = true;
 
-    constexpr static const OverTalonFXConfig CenteringConfig() { //Limites cuestionables
-        OverTalonFXConfig centeringConfig;
-        centeringConfig.MotorId = CenteringMotorId;
-        centeringConfig.NeutralMode = ControllerNeutralMode::Brake;
-        centeringConfig.Inverted = true;
+	rollersConfig.CurrentLimit = 25_A;
+	rollersConfig.StatorCurrentLimit = 120_A;
+	rollersConfig.TriggerThreshold = 40_A;
+	rollersConfig.TriggerThresholdTime = 0.5_s;
+	rollersConfig.ClosedLoopRampRate = 0.0_s;
+	rollersConfig.OpenLoopRampRate = 0.05_s;
 
-        centeringConfig.CurrentLimit = 25_A;
-        centeringConfig.StatorCurrentLimit = 120_A;
-        centeringConfig.TriggerThreshold = 40_A;
-        centeringConfig.TriggerThresholdTime = 0.5_s;
-        centeringConfig.ClosedLoopRampRate = 0.0_s;
-        centeringConfig.OpenLoopRampRate = 0.05_s;
+	return rollersConfig;
+}
 
-        return centeringConfig;
-    }
+constexpr static const OverTalonFXConfig CenteringConfig() { //Limites cuestionables
+	OverTalonFXConfig centeringConfig;
+	centeringConfig.MotorId = CenteringMotorId;
+	centeringConfig.NeutralMode = ControllerNeutralMode::Brake;
+	centeringConfig.Inverted = true;
+
+	centeringConfig.CurrentLimit = 25_A;
+	centeringConfig.StatorCurrentLimit = 120_A;
+	centeringConfig.TriggerThreshold = 40_A;
+	centeringConfig.TriggerThresholdTime = 0.5_s;
+	centeringConfig.ClosedLoopRampRate = 0.0_s;
+	centeringConfig.OpenLoopRampRate = 0.05_s;
+
+	return centeringConfig;
+}
 
 };

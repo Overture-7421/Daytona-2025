22347f94d5a1929a91d0380d43f635b12fa1f596
EricBrwn <a01572515@tec.mx>
2025-08-22 19:49:07 -0600

    Spotless , and )

--- src/main/Manager/StateManager/StateManager.h
+++ src/main/Manager/StateManager/StateManager.h
@@ -110,124 +110,68 @@ private:
             arm->setState(Positions::EndPosition), elevator->setState(Positions::EndPosition),
             intake->setState(Positions::EndPosition), grabber->setState(Positions::EndPosition),
             climber->setState(Positions::EndPosition))}, {Positions::L1Position, frc2::cmd::Parallel(
-        frc2::cmd::Sequence(
-        arm->setState(Positions::L1Position),
-        elevator->setState(Positions::L1Position)),
-        frc2::cmd::Parallel(
-        intake->setState(Positions::L1Position),
-        grabber->setState(Positions::L1Position),
-        climber->setState(Positions::L1Position))
-      )},
+            frc2::cmd::Sequence(arm->setState(Positions::L1Position), elevator->setState(Positions::L1Position)),
+            frc2::cmd::Parallel(intake->setState(Positions::L1Position), grabber->setState(Positions::L1Position),
+                    climber->setState(Positions::L1Position)))},
 
-      {Positions::L1Confirm, frc2::cmd::Parallel(
-        elevator->setState(Positions::L1Confirm),
-        arm->setState(Positions::L1Confirm),
-        intake->setState(Positions::L1Confirm),
-        grabber->setState(Positions::L1Confirm),
-        climber->setState(Positions::L1Confirm)
-      )},
+    {Positions::L1Confirm, frc2::cmd::Parallel(elevator->setState(Positions::L1Confirm),
+            arm->setState(Positions::L1Confirm), intake->setState(Positions::L1Confirm),
+            grabber->setState(Positions::L1Confirm), climber->setState(Positions::L1Confirm))},
 
-      {Positions::L2Front, frc2::cmd::Parallel(
-        frc2::cmd::Sequence(
-        arm->setState(Positions::L2Front),
-        elevator->setState(Positions::L2Front,Heading::Front)),
-        frc2::cmd::Parallel(
-        intake->setState(Positions::L2Front),
-        grabber->setState(Positions::L2Front),
-        climber->setState(Positions::L2Front))
-        )
-      },
+    {Positions::L2Front, frc2::cmd::Parallel(
+            frc2::cmd::Sequence(arm->setState(Positions::L2Front, Heading::Front),
+                    elevator->setState(Positions::L2Front)),
+            frc2::cmd::Parallel(intake->setState(Positions::L2Front), grabber->setState(Positions::L2Front),
+                    climber->setState(Positions::L2Front)))},
 
-      {Positions::L2FrontConfirm, frc2::cmd::Parallel(
-        arm->setState(Positions::L2FrontConfirm),
-        elevator->setState(Positions::L2FrontConfirm,Heading::Front)),
-        intake->setState(Positions::L2FrontConfirm),
-        grabber->setState(Positions::L2FrontConfirm),
-        climber->setState(Positions::L2FrontConfirm)
-      },
+    {Positions::L2FrontConfirm, frc2::cmd::Parallel(arm->setState(Positions::L2FrontConfirm, Heading::Front),
+            elevator->setState(Positions::L2FrontConfirm), intake->setState(Positions::L2FrontConfirm),
+            grabber->setState(Positions::L2FrontConfirm), climber->setState(Positions::L2FrontConfirm))},
 
-      {Positions::L2Back, frc2::cmd::Parallel(
-        frc2::cmd::Sequence(
-        arm->setState(Positions::L2Back),
-        elevator->setState(Positions::L2Back,Heading::Back)),
-        frc2::cmd::Parallel(
-        intake->setState(Positions::L2Back),
-        grabber->setState(Positions::L2Back),
-        climber->setState(Positions::L2Back))
-        )
-      },
+    {Positions::L2Back, frc2::cmd::Parallel(
+            frc2::cmd::Sequence(arm->setState(Positions::L2Back, Heading::Back), elevator->setState(Positions::L2Back)),
+            frc2::cmd::Parallel(intake->setState(Positions::L2Back), grabber->setState(Positions::L2Back),
+                    climber->setState(Positions::L2Back)))},
 
-      {Positions::L2BackConfirm, frc2::cmd::Parallel(
-        arm->setState(Positions::L2BackConfirm),
-        elevator->setState(Positions::L2BackConfirm,Heading::Back)),
-        intake->setState(Positions::L2BackConfirm),
-        grabber->setState(Positions::L2BackConfirm),
-        climber->setState(Positions::L2BackConfirm)
-      },
+    {Positions::L2BackConfirm, frc2::cmd::Parallel(arm->setState(Positions::L2BackConfirm, Heading::Back),
+            elevator->setState(Positions::L2BackConfirm), intake->setState(Positions::L2BackConfirm),
+            grabber->setState(Positions::L2BackConfirm), climber->setState(Positions::L2BackConfirm))},
 
-      {Positions::L3Front, frc2::cmd::Parallel(
-        arm->setState(Positions::L3Front),
-        elevator->setState(Positions::L3Front,Heading::Front)),
-        intake->setState(Positions::L3Front),
-        grabber->setState(Positions::L3Front),
-        climber->setState(Positions::L3Front)
-      },
+    {Positions::L3Front, frc2::cmd::Parallel(arm->setState(Positions::L3Front, Heading::Front),
+            elevator->setState(Positions::L3Front), intake->setState(Positions::L3Front),
+            grabber->setState(Positions::L3Front), climber->setState(Positions::L3Front))},
 
-      {Positions::L3FrontConfirm, frc2::cmd::Parallel(
-        arm->setState(Positions::L3FrontConfirm),
-        elevator->setState(Positions::L3FrontConfirm,Heading::Front)),
-        intake->setState(Positions::L3FrontConfirm),
-        grabber->setState(Positions::L3FrontConfirm),
-        climber->setState(Positions::L3FrontConfirm)
-      },
+    {Positions::L3FrontConfirm, frc2::cmd::Parallel(arm->setState(Positions::L3FrontConfirm, Heading::Front),
+            elevator->setState(Positions::L3FrontConfirm), intake->setState(Positions::L3FrontConfirm),
+            grabber->setState(Positions::L3FrontConfirm), climber->setState(Positions::L3FrontConfirm))},
 
-      {Positions::L3Back, frc2::cmd::Parallel(
-        arm->setState(Positions::L3Back),
-        elevator->setState(Positions::L3Back,Heading::Back)),
-        intake->setState(Positions::L3Back),
-        grabber->setState(Positions::L3Back),
-        climber->setState(Positions::L3Back)
-      },
+    {Positions::L3Back, frc2::cmd::Parallel(arm->setState(Positions::L3Back, Heading::Back),
+            elevator->setState(Positions::L3Back), intake->setState(Positions::L3Back),
+            grabber->setState(Positions::L3Back), climber->setState(Positions::L3Back))},
 
-      {Positions::L3BackConfirm, frc2::cmd::Parallel(
-        arm->setState(Positions::L3BackConfirm),
-        elevator->setState(Positions::L3BackConfirm,Heading::Back)),
-        intake->setState(Positions::L3BackConfirm),
-        grabber->setState(Positions::L3BackConfirm),
-        climber->setState(Positions::L3BackConfirm)
-      },
+    {Positions::L3BackConfirm, frc2::cmd::Parallel(arm->setState(Positions::L3BackConfirm, Heading::Back),
+            elevator->setState(Positions::L3BackConfirm), intake->setState(Positions::L3BackConfirm),
+            grabber->setState(Positions::L3BackConfirm), climber->setState(Positions::L3BackConfirm))},
 
     {Positions::L4Front, frc2::cmd::Parallel(
-        arm->setState(Positions::L4Front),
-        elevator->setState(Positions::L4Front,Heading::Front)),
-        intake->setState(Positions::L4Front),
-        grabber->setState(Positions::L4Front),
-        climber->setState(Positions::L4Front)
-      },
 
-      {Positions::L4FrontConfirm, frc2::cmd::Parallel(
-        arm->setState(Positions::L4FrontConfirm),
-        elevator->setState(Positions::L4FrontConfirm,Heading::Front)),
-        intake->setState(Positions::L4FrontConfirm),
-        grabber->setState(Positions::L4FrontConfirm),
-        climber->setState(Positions::L4FrontConfirm)
-      },
+    arm->setState(Positions::L4Front, Heading::Front), elevator->setState(Positions::L4Front)
 
-      {Positions::L4Back, frc2::cmd::Parallel(
-        arm->setState(Positions::L4Back),
-        elevator->setState(Positions::L4Back,Heading::Back)),
-        intake->setState(Positions::L4Back),
-        grabber->setState(Positions::L4Back),
-        climber->setState(Positions::L4Back)
-      },
+    , intake->setState(Positions::L4Front), grabber->setState(Positions::L4Front),
+            climber->setState(Positions::L4Front))},
 
-      {Positions::L4BackConfirm, frc2::cmd::Parallel(
-        arm->setState(Positions::L4BackConfirm),
-        elevator->setState(Positions::L4BackConfirm,Heading::Back)),
-        intake->setState(Positions::L4BackConfirm),
-        grabber->setState(Positions::L4BackConfirm),
-        climber->setState(Positions::L4BackConfirm)
-      },
+    {Positions::L4FrontConfirm, frc2::cmd::Parallel(arm->setState(Positions::L4FrontConfirm, Heading::Front),
+            elevator->setState(Positions::L4FrontConfirm), intake->setState(Positions::L4FrontConfirm),
+            grabber->setState(Positions::L4FrontConfirm), climber->setState(Positions::L4FrontConfirm))},
 
+    {Positions::L4Back, frc2::cmd::Parallel(arm->setState(Positions::L4Back, Heading::Back),
+            elevator->setState(Positions::L4Back), intake->setState(Positions::L4Back),
+            grabber->setState(Positions::L4Back), climber->setState(Positions::L4Back))},
+
+    {Positions::L4BackConfirm, frc2::cmd::Parallel(arm->setState(Positions::L4BackConfirm, Heading::Back),
+            elevator->setState(Positions::L4BackConfirm), intake->setState(Positions::L4BackConfirm),
+            grabber->setState(Positions::L4BackConfirm), climber->setState(Positions::L4BackConfirm))}
+
+    };
 
 };

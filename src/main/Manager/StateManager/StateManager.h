11ad9730bab900a34a2f37b36a8626a9cd56bd10
EricBrwn <a01572515@tec.mx>
2025-08-22 19:19:13 -0600

    PositionsNotCoralReef finished and also algorithm to set states

--- src/main/Manager/StateManager/StateManager.h
+++ src/main/Manager/StateManager/StateManager.h
@@ -64,10 +64,53 @@ private:
 
     };
 
-    std::map<Positions, const frc2::CommandPtr&> positionsMap { 
-      
+    std::map<Positions, const frc2::CommandPtr&> positionsMap { {Positions::InitialPosition, frc2::cmd::Parallel(
+            elevator->setState(Positions::InitialPosition), arm->setState(Positions::InitialPosition),
+            intake->setState(Positions::InitialPosition), grabber->setState(Positions::InitialPosition),
+            climber->setState(Positions::InitialPosition))}, {Positions::SustainedPosition, frc2::cmd::Sequence(
+            elevator->setState(Positions::SustainedPosition), arm->setState(Positions::SustainedPosition),
+            intake->setState(Positions::SustainedPosition), grabber->setState(Positions::SustainedPosition),
+            climber->setState(Positions::SustainedPosition))}, {Positions::Intake, frc2::cmd::Sequence(
+            intake->setState(Positions::Intake), elevator->setState(Positions::Intake),
+            arm->setState(Positions::Intake), grabber->setState(Positions::Intake),
+            climber->setState(Positions::Intake))}, {Positions::IntakeCoralStation, frc2::cmd::Sequence(
+            intake->setState(Positions::IntakeCoralStation), elevator->setState(Positions::IntakeCoralStation),
+            arm->setState(Positions::IntakeCoralStation), grabber->setState(Positions::IntakeCoralStation),
+            climber->setState(Positions::IntakeCoralStation))}, {Positions::AlgaeHighReef, frc2::cmd::Sequence(
+            elevator->setState(Positions::AlgaeHighReef), arm->setState(Positions::AlgaeHighReef),
+            grabber->setState(Positions::AlgaeHighReef), intake->setState(Positions::AlgaeHighReef),
+            climber->setState(Positions::AlgaeHighReef))}, {Positions::AlgaeLowReef, frc2::cmd::Sequence(
+            elevator->setState(Positions::AlgaeLowReef), arm->setState(Positions::AlgaeLowReef),
+            grabber->setState(Positions::AlgaeLowReef), intake->setState(Positions::AlgaeLowReef),
+            climber->setState(Positions::AlgaeLowReef))}, {Positions::AlgaeGround, frc2::cmd::Sequence(
+            intake->setState(Positions::AlgaeGround), arm->setState(Positions::AlgaeGround),
+            elevator->setState(Positions::AlgaeGround), grabber->setState(Positions::AlgaeGround),
+            climber->setState(Positions::AlgaeGround))}, {Positions::CoralHold, frc2::cmd::Sequence(
+            intake->setState(Positions::CoralHold), arm->setState(Positions::CoralHold),
+            elevator->setState(Positions::CoralHold), grabber->setState(Positions::CoralHold),
+            climber->setState(Positions::CoralHold))}, {Positions::CoralAndAlgae, frc2::cmd::Sequence(
+            elevator->setState(Positions::CoralAndAlgae), arm->setState(Positions::CoralAndAlgae),
+            intake->setState(Positions::CoralAndAlgae), grabber->setState(Positions::CoralAndAlgae),
+            climber->setState(Positions::CoralAndAlgae))}, {Positions::AlgaeHold, frc2::cmd::Sequence(
+            arm->setState(Positions::AlgaeHold), elevator->setState(Positions::AlgaeHold),
+            intake->setState(Positions::AlgaeHold), grabber->setState(Positions::AlgaeHold),
+            climber->setState(Positions::AlgaeHold))}, {Positions::NetPosition, frc2::cmd::Sequence(
+            arm->setState(Positions::NetPosition), elevator->setState(Positions::NetPosition),
+            grabber->setState(Positions::NetPosition), intake->setState(Positions::NetPosition),
+            climber->setState(Positions::NetPosition))}, {Positions::NetConfirm, frc2::cmd::Sequence(
+            grabber->setState(Positions::NetConfirm), arm->setState(Positions::NetConfirm),
+            elevator->setState(Positions::NetConfirm), intake->setState(Positions::NetConfirm),
+            climber->setState(Positions::NetConfirm))}, {Positions::ProcessorPosition, frc2::cmd::Sequence(
+            intake->setState(Positions::ProcessorPosition), arm->setState(Positions::ProcessorPosition),
+            elevator->setState(Positions::ProcessorPosition), grabber->setState(Positions::ProcessorPosition),
+            climber->setState(Positions::ProcessorPosition))}, {Positions::ProcessorConfirm, frc2::cmd::Sequence(
+            grabber->setState(Positions::ProcessorConfirm), intake->setState(Positions::ProcessorConfirm),
+            arm->setState(Positions::ProcessorConfirm), elevator->setState(Positions::ProcessorConfirm),
+            climber->setState(Positions::ProcessorConfirm))}, {Positions::EndPosition, frc2::cmd::Sequence(
+            arm->setState(Positions::EndPosition), elevator->setState(Positions::EndPosition),
+            intake->setState(Positions::EndPosition), grabber->setState(Positions::EndPosition),
+            climber->setState(Positions::EndPosition))}
 
-      
     };
 
 };

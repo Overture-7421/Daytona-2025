d4d6d14f23449015b721c609cfb1e0416cfeca77
EricBrwn <a01572515@tec.mx>
2025-09-10 00:03:59 -0600

    cameras

--- src/main/RobotContainer.cpp
+++ src/main/RobotContainer.cpp
@@ -236,7 +236,7 @@ void RobotContainer::ConfigCharacterizationBindings() {
 AprilTags::Config RobotContainer::railCameraRight() {
     AprilTags::Config config;
     config.cameraName = "RailRight";
-    config.cameraToRobot = {8_in, -9.2_in, 11.52224_in, {0_deg, -5_deg, -44.124984_deg}};
+    config.cameraToRobot = {11.2_in, 3.5_in, 7.752224_in, {0_deg, -15_deg, 47.981360_deg}};
     config.tagValidDistances = { {1, 3.5_m}, {2, 4.0_m}, {3, 4.0_m}};
     return config;
 }
@@ -245,7 +245,7 @@ AprilTags::Config RobotContainer::railCameraRight() {
 AprilTags::Config RobotContainer::climberCameraLeft() {
     AprilTags::Config config;
     config.cameraName = "ClimberLeft";
-    config.cameraToRobot = {-11.162644_in, 10.651305_in, 8.845276_in, {0_deg, -20_deg, 125.531348_deg}};
+    config.cameraToRobot = {-11.162644_in, 10.651305_in, 8.845276_in, {0_deg, -20_deg, 60.024706_deg}};
     config.tagValidDistances = { {1, 3.5_m}, {2, 4.0_m}, {3, 4.0_m}};
     return config;
 }
@@ -253,14 +253,14 @@ AprilTags::Config RobotContainer::climberCameraLeft() {
 AprilTags::Config RobotContainer::climberCameraRight() {
     AprilTags::Config config;
     config.cameraName = "ClimberRight";
-    config.cameraToRobot = {-11.4_in, -1.319890_in, 6.843439_in, {0_deg, -15_deg, 146.355913_deg}};
+    config.cameraToRobot = {-11.4_in, -1.319890_in, 6.843439_in, {0_deg, -15_deg, 35.018739_deg}};
     return config;
 }
 
 AprilTags::Config RobotContainer::railCameraLeft() {
     AprilTags::Config config;
     config.cameraName = "RailLeft";
-    config.cameraToRobot = {11.2_in, -3.5_in, 7.752_in, {0_deg, -15_deg, -30.375507_deg}};
+    config.cameraToRobot = {8_in, 9.2_in, 11.252224_in, {0_deg, -5_deg, 29.993788_deg}};
     return config;
 }
 void RobotContainer::UpdateTelemetry() {

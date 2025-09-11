f93c9656122d4886d5e40b044d31593a1ccb2d06
Luis Leyva <luisoleyvan@gmail.com>
2025-09-11 00:57:41 -0600

    Working Auto

--- src/main/Manager/AlignManager/AlignManager.cpp
+++ src/main/Manager/AlignManager/AlignManager.cpp
@@ -4,76 +4,119 @@
 
 #include "AlignManager.h"
 
-AlignManager::AlignManager(Chassis *chassis, frc::AprilTagFieldLayout *tagLayout) {
-    this->chassis = chassis;
-    this->tagLayout = tagLayout;
+AlignManager::AlignManager(Chassis* chassis, frc::AprilTagFieldLayout* tagLayout) {
+	this->chassis = chassis;
+	this->tagLayout = tagLayout;
+
+	this->xPIDController.SetIZone(3);
+	this->xPIDController.SetTolerance(0.01_m);
+	this->yPIDController.SetIZone(3);
+	this->yPIDController.SetTolerance(0.01_m);
+	this->headingPIDController.SetIZone(3);
+	this->headingPIDController.SetTolerance(0.5_deg);
+	this->headingPIDController.EnableContinuousInput(-180_deg, 180_deg);
 }
 
-void AlignManager::getReefOffset(ReefSide reefSide) {
-    ReefPackage reefPackage = findClosestReefLocation(chassis, tagLayout);
-    if (reefPackage.alliance == frc::DriverStation::Alliance::kRed) {
-        alignPositionsMap = alignInRed;
-    } else {
-        alignPositionsMap = alignInBlue;
-    }
+void AlignManager::initialize() {
+	ReefPackage reefPackage = findClosestReefLocation(chassis, tagLayout);
+	if (reefPackage.alliance == frc::DriverStation::Alliance::kRed) {
+		alignPositionsMap = alignInRed;
+	} else {
+		alignPositionsMap = alignInBlue;
+	}
 
-    if (alignPositionsMap.contains(reefPackage.reefLocation)) {
-        reefOffset = alignPositionsMap.at(reefPackage.reefLocation);
-    } else {
-        reefOffset = defaultReefOffset;
-    }
+	if (alignPositionsMap.contains(reefPackage.reefLocation)) {
+		reefOffset = alignPositionsMap.at(reefPackage.reefLocation);
+	} else {
+		reefOffset = defaultReefOffset;
+	}
 
-    if (reefSide == ReefSide::Left) {
-        yTarget = reefOffset.leftOffset;
-    } else if (reefSide == ReefSide::Right) {
-        yTarget = reefOffset.rightOffset;
-    } else {
-        yTarget = reefOffset.algaeOffset;
-    }
+	if (reefSide == ReefSide::Left) {
+		yTarget = reefOffset.leftOffset;
+	} else if (reefSide == ReefSide::Right) {
+		yTarget = reefOffset.rightOffset;
+	} else {
+		yTarget = reefOffset.algaeOffset;
+	}
 
-    if (reefPackage.algaePose == AlgaePose::Up) {
-        setAlgaePose(AlgaePose::Up);
-    } else if (reefPackage.algaePose == AlgaePose::Down) {
-        setAlgaePose(AlgaePose::Down);
-    }
+	if (reefPackage.algaePose == AlgaePose::Up) {
+		setAlgaePose(AlgaePose::Up);
+	} else if (reefPackage.algaePose == AlgaePose::Down) {
+		setAlgaePose(AlgaePose::Down);
+	}
 
-    units::degree_t chassisHeading = chassis->getEstimatedPose().RelativeTo(reefPackage.pose).Rotation().Degrees();
-    if (chassisHeading < 90_deg || chassisHeading > -90_deg) {
-        headingTarget = reefOffset.headingOffset;
-        setHeading(Heading::Front);
-    } else {
-        headingTarget = reefOffset.headingOffset + 180_deg;
-        setHeading(Heading::Back);
-    }
+	units::degree_t chassisHeading = chassis->getEstimatedPose().RelativeTo(reefPackage.pose).Rotation().Degrees();
+	if (chassisHeading < 90_deg || chassisHeading > -90_deg) {
+		headingTarget = reefOffset.headingOffset;
+		setHeading(Heading::Front);
+	} else {
+		headingTarget = reefOffset.headingOffset + 180_deg;
+		setHeading(Heading::Back);
+	}
 
-    targetPose = reefPackage.pose.TransformBy( {reefOffset.xOffset, yTarget, headingTarget});
+	targetPose = reefPackage.pose.TransformBy({ reefOffset.xOffset, yTarget, headingTarget });
+
+	frc::Pose2d pose = chassis->getEstimatedPose();
+
+	frc::ChassisSpeeds currentSpeeds = frc::ChassisSpeeds::FromFieldRelativeSpeeds(chassis->getCurrentSpeeds(),
+		-pose.Rotation() + reefPackage.pose.Rotation());
+	xPIDController.Reset(pose.X(), currentSpeeds.vx);
+	yPIDController.Reset(pose.Y(), currentSpeeds.vy);
+	headingPIDController.Reset(pose.Rotation().Degrees(), currentSpeeds.omega);
 }
 
-pathplanner::PathConstraints constraints = pathplanner::PathConstraints(3.5_mps, 2.25_mps_sq, 540_deg_per_s,
-	720_deg_per_s_sq);
+void AlignManager::alterSpeed(frc::ChassisSpeeds& inputSpeed) {
+	frc::Pose2d pose = chassis->getEstimatedPose();
+
+	auto xSpeed = xPIDController.Calculate(pose.X(), targetPose.X()) * 1_mps;
+	auto ySpeed = yPIDController.Calculate(pose.Y(), targetPose.Y()) * 1_mps;
+	auto headingSpeed = headingPIDController.Calculate(pose.Rotation().Degrees(), targetPose.Rotation().Degrees()) * 1_deg_per_s;
+
+	if (xPIDController.AtGoal()) {
+		xSpeed = 0_mps;
+	}
+	if (yPIDController.AtGoal()) {
+		ySpeed = 0_mps;
+	}
+	if (headingPIDController.AtGoal()) {
+		headingSpeed = 0_deg_per_s;
+	}
+
+	inputSpeed = frc::ChassisSpeeds::FromFieldRelativeSpeeds(xSpeed, ySpeed,
+		units::radians_per_second_t(headingSpeed), pose.Rotation());
+}
+
 
 frc2::CommandPtr AlignManager::AlignToPose(ReefSide reefSide) {
-    return frc2::cmd::Sequence(frc2::cmd::RunOnce([this, reefSide] {
-        getReefOffset(reefSide);
-    }),
-    frc2::cmd::Defer([this]() {
-        return pathplanner::AutoBuilder::pathfindToPose(targetPose, constraints, 0_mps);
-    }, {chassis})  // Add chassis as requirement
-            );
+	return frc2::FunctionalCommand(
+		[this, reefSide]() {
+		this->reefSide = reefSide;
+		this->initialize();
+		this->chassis->enableSpeedHelper(this);
+	},
+		[this]() {},
+		[this](bool interrupted) {
+		this->chassis->disableSpeedHelper();
+	},
+		[this]() {
+		return xPIDController.AtGoal() && yPIDController.AtGoal() && headingPIDController.AtGoal();
+	},
+		{ chassis }
+	).ToPtr();
 }
 
 void AlignManager::setHeading(Heading heading) {
-    this->heading = heading;
+	this->heading = heading;
 }
 
 Heading AlignManager::getHeading() {
-    return heading;
+	return heading;
 }
 
 void AlignManager::setAlgaePose(AlgaePose algaePose) {
-    this->algaePose = algaePose;
+	this->algaePose = algaePose;
 }
 
 AlgaePose AlignManager::getAlgaePose() {
-    return algaePose;
+	return algaePose;
 }
--- src/main/Manager/AlignManager/AlignManager.h
+++ src/main/Manager/AlignManager/AlignManager.h
@@ -6,7 +6,7 @@
 
 #include "Subsystems/Chassis/Chassis.h"
 #include <frc/smartdashboard/SmartDashboard.h>
-
+#include <frc/controller/ProfiledPIDController.h>
 #include "Commands/UtilityFunctions/UtilityFunctions.h"
 #include <map>
 #include "Enums/ReefSide.h"
@@ -14,46 +14,56 @@
 #include "Enums/Heading.h"
 
 #include <frc2/command/CommandPtr.h>
-#include <pathplanner/lib/auto/AutoBuilder.h>
-#include "OvertureLib/Utils/Logging/Logging.h"
+#include "OvertureLib/Subsystems/Swerve/SpeedsHelper/SpeedsHelper.h"
 
-class AlignManager {
+class AlignManager : public SpeedsHelper {
 public:
-    AlignManager(Chassis *chassis, frc::AprilTagFieldLayout *tagLayout);
+	AlignManager(Chassis* chassis, frc::AprilTagFieldLayout* tagLayout);
+	void initialize() override;
+	void alterSpeed(frc::ChassisSpeeds& inputSpeed) override;
 
-    void getReefOffset(ReefSide reefSide);
+	void setHeading(Heading heading);
+	Heading getHeading();
 
-    frc2::CommandPtr AlignToPose(ReefSide reefSide);
+	void setAlgaePose(AlgaePose algaePose);
+	AlgaePose getAlgaePose();
 
-    void setHeading(Heading heading);
-    Heading getHeading();
+	frc2::CommandPtr AlignToPose(ReefSide reefSide);
 
-    void setAlgaePose(AlgaePose algaePose);
-    AlgaePose getAlgaePose();
-
-    //MOVER DERECHA es mas POSITIVOS / MOVER IZQUIERDA es mas NEGATIVOS 
-    //ATRAS es mas POSITIVOS / ADELANTE es mas NEGATIVOS
-    //GIRAR IZQUIERDA mas POSITIVOS / GIRAR DERECHA mas NEGATIVOS
+	//MOVER DERECHA es mas POSITIVOS / MOVER IZQUIERDA es mas NEGATIVOS 
+	//ATRAS es mas POSITIVOS / ADELANTE es mas NEGATIVOS
+	//GIRAR IZQUIERDA mas POSITIVOS / GIRAR DERECHA mas NEGATIVOS
 
 	//IZQUIERDA, DERECHA, ADELANTE/ATRAS, ROTACION, ALGA
 	const ReefOffset defaultReefOffset{ -0.25_m, 0.085_m, 0.51_m, 180.0_deg, 0.0_m };
 
-    std::map<ReefLocation, ReefOffset> alignPositionsMap;
-    const std::map<ReefLocation, ReefOffset> alignInRed = {};
-    const std::map<ReefLocation, ReefOffset> alignInBlue = {};
+	std::map<ReefLocation, ReefOffset> alignPositionsMap;
+	const std::map<ReefLocation, ReefOffset> alignInRed = {};
+	const std::map<ReefLocation, ReefOffset> alignInBlue = {};
 
 private:
-    Chassis *chassis = nullptr;
-    frc::AprilTagFieldLayout *tagLayout = nullptr;
+	ReefSide reefSide;
+	Chassis* chassis = nullptr;
+	frc::AprilTagFieldLayout* tagLayout = nullptr;
 
-    ReefOffset reefOffset;
+	frc::TrapezoidProfile<units::meters>::Constraints defaultConstraints{ 2_mps, 1_mps_sq };
 
-    units::meter_t xTarget = 0.0_m; // The target X position in the target frame
-    units::meter_t yTarget = 0.0_m; // The target Y position in the target frame
-    units::degree_t headingTarget = 0.0_deg; // The target heading in the target frame
+	frc::TrapezoidProfile<units::meters>::Constraints minimumConstraints{ 1_mps, 0.25_mps_sq };
 
-    frc::Pose2d targetPose;
+	frc::ProfiledPIDController<units::meters> xPIDController{ 10, 0.0, 0.0, defaultConstraints }; //4 2.1
 
-    Heading heading = Heading::Front;
-    AlgaePose algaePose = AlgaePose::Up;
+	frc::ProfiledPIDController<units::meters> yPIDController{ 10, 0.0, 0.0, defaultConstraints }; //4 2.1
+
+	frc::ProfiledPIDController<units::degree> headingPIDController{ 5.55, 0.0, 0.0, {200_deg_per_s, 125_deg_per_s / 1_s} };
+
+	ReefOffset reefOffset;
+
+	units::meter_t xTarget = 0.0_m; // The target X position in the target frame
+	units::meter_t yTarget = 0.0_m; // The target Y position in the target frame
+	units::degree_t headingTarget = 0.0_deg; // The target heading in the target frame
+
+	frc::Pose2d targetPose;
+
+	Heading heading = Heading::Front;
+	AlgaePose algaePose = AlgaePose::Up;
 };

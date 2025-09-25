87e3175e856ecd63505c8b22b611fb85254d3c78
Luis Leyva <luisoleyvan@gmail.com>
2025-09-25 15:19:56 -0600

    Re arange for back implementation (#107)
    * New changes
    
    * FIxed???????
    
    * WOrks???????
    
    * Works

--- src/main/Manager/AlignManager/AlignManager.cpp
+++ src/main/Manager/AlignManager/AlignManager.cpp
@@ -4,140 +4,62 @@
 
 #include "AlignManager.h"
 
-AlignManager::AlignManager(Chassis *chassis, frc::AprilTagFieldLayout *tagLayout) {
+AlignManager::AlignManager(Chassis* chassis, frc::AprilTagFieldLayout* tagLayout) {
     this->chassis = chassis;
     this->tagLayout = tagLayout;
+}
 
-    this->xPIDController.SetIZone(3);
-    this->xPIDController.SetTolerance(0.025_m);
-    this->yPIDController.SetIZone(3);
-    this->yPIDController.SetTolerance(0.025_m);
-    this->headingPIDController.SetIZone(3);
-    this->headingPIDController.SetTolerance(4.0_deg);
-    this->headingPIDController.EnableContinuousInput(-180_deg, 180_deg);
+Heading AlignManager::getHeading() {
+    return this->heading;
 }
 
 void AlignManager::initialize() {
+
+    if (this->heading == Heading::Back) {
+        headingTarget = backReefOffset.headingOffset;
+        this->reefOffset = backReefOffset;
+    } else {
+        headingTarget = frontReefOffset.headingOffset;
+        this->reefOffset = frontReefOffset;
+    }
+
+    this->alignSpeedHelper = std::make_shared < AlignSpeedHelper
+    >(chassis, tagLayout, reefSide, reefPackage, this->reefOffset, headingTarget);
+    this->alignSpeedHelper->initialize();
+    this->chassis->enableSpeedHelper(alignSpeedHelper.get());
+}
+
+void AlignManager::setHeading() {
     reefPackage = findClosestReefLocation(chassis, tagLayout);
-
-    // units::degree_t chassisHeading = chassis->getEstimatedPose().RelativeTo(reefPackage.pose).Rotation().Degrees();
-    // if (chassisHeading > 90_deg || chassisHeading < -90_deg) {
-    //     setHeading(Heading::Front);
-    // } else {
-    //     setHeading(Heading::Back);
-    // }
-
-    if (reefPackage.alliance == frc::DriverStation::Alliance::kRed && getHeading() == Heading::Front) {
-        alignPositionsMap = frontAlignInRed;
-    } else if (reefPackage.alliance == frc::DriverStation::Alliance::kBlue && getHeading() == Heading::Front) {
-        alignPositionsMap = frontAlignInBlue;
-    } else if (reefPackage.alliance == frc::DriverStation::Alliance::kRed && getHeading() == Heading::Back) {
-        alignPositionsMap = backAlignInRed;
-    } else if (reefPackage.alliance == frc::DriverStation::Alliance::kBlue && getHeading() == Heading::Back) {
-        alignPositionsMap = backAlignInBlue;
-    }
-
-    if (alignPositionsMap.contains(reefPackage.reefLocation)) {
-        reefOffset = alignPositionsMap.at(reefPackage.reefLocation);
+    units::degree_t chassisHeading = chassis->getEstimatedPose().RelativeTo(reefPackage.pose).Rotation().Degrees();
+    if (chassisHeading > 90_deg || chassisHeading < -90_deg) {
+        this->heading = Heading::Front;
     } else {
-        // if (getHeading() == Heading::Back) {
-        //     headingTarget = backReefOffset.headingOffset;
-        // reefOffset = backReefOffset;
-        // } else {
-        // headingTarget = frontReefOffset.headingOffset;
-        reefOffset = frontReefOffset;
-        // }
+        this->heading = Heading::Back;
     }
-    headingTarget = frontReefOffset.headingOffset;
-    setHeading(Heading::Front);
-
-    if (reefSide == ReefSide::Left) {
-        yTarget = reefOffset.leftOffset;
-    } else if (reefSide == ReefSide::Right) {
-        yTarget = reefOffset.rightOffset;
-    } else {
-        yTarget = reefOffset.algaeOffset;
-    }
-
-    xTarget = reefOffset.xOffset;
-
-    if (reefPackage.algaePose == AlgaePose::Up) {
-        setAlgaePose(AlgaePose::Up);
-    } else if (reefPackage.algaePose == AlgaePose::Down) {
-        setAlgaePose(AlgaePose::Down);
-    }
-
-    frc::Pose2d pose = chassis->getEstimatedPose();
-    frc::Pose2d poseInTargetFrame = transformToTargetFrame(pose);
-
-    frc::ChassisSpeeds currentSpeeds = frc::ChassisSpeeds::FromFieldRelativeSpeeds(chassis->getCurrentSpeeds(),
-            -pose.Rotation() + reefPackage.pose.Rotation());
-    xPIDController.Reset(poseInTargetFrame.X(), currentSpeeds.vx);
-    yPIDController.Reset(poseInTargetFrame.Y(), currentSpeeds.vy);
-    headingPIDController.Reset(poseInTargetFrame.Rotation().Degrees(), currentSpeeds.omega);
-
-    frc::SmartDashboard::PutNumber("Align/TargetX", xTarget.value());
-    frc::SmartDashboard::PutNumber("Align/TargetY", yTarget.value());
-    frc::SmartDashboard::PutNumber("Align/TargetHeading", headingTarget.value());
 }
 
-void AlignManager::alterSpeed(frc::ChassisSpeeds &inputSpeed) {
-    frc::Pose2d pose = chassis->getEstimatedPose();
-
-    frc::Pose2d poseInTargetFrame = transformToTargetFrame(pose);
-
-    auto xSpeed = xPIDController.Calculate(poseInTargetFrame.X(), xTarget) * 1_mps;
-    auto ySpeed = yPIDController.Calculate(poseInTargetFrame.Y(), yTarget) * 1_mps;
-    auto headingSpeed = headingPIDController.Calculate(poseInTargetFrame.Rotation().Degrees(), headingTarget)
-            * 1_deg_per_s;
-
-    if (xPIDController.AtGoal()) {
-        xSpeed = 0_mps;
-    }
-    if (yPIDController.AtGoal()) {
-        ySpeed = 0_mps;
-    }
-    if (headingPIDController.AtGoal()) {
-        headingSpeed = 0_deg_per_s;
-    }
-
-    inputSpeed = frc::ChassisSpeeds::FromFieldRelativeSpeeds(xSpeed, ySpeed, units::radians_per_second_t(headingSpeed),
-            pose.Rotation() - reefPackage.pose.Rotation());
-}
-
-frc::Pose2d AlignManager::transformToTargetFrame(const frc::Pose2d &pose) {
-    return pose.RelativeTo(reefPackage.pose);
+void AlignManager::resetSpeedHelper() {
+	if (this->alignSpeedHelper) {
+		this->chassis->disableSpeedHelper();
+		this->alignSpeedHelper.reset();
+	}
 }
 
 frc2::CommandPtr AlignManager::AlignToPose(ReefSide reefSide) {
     return frc2::FunctionalCommand([this, reefSide]() {
         this->reefSide = reefSide;
-        this->initialize();
-        this->chassis->enableSpeedHelper(this);
+
+        initialize();
     },
-    [this]() {
+        [this]() {
+        // Execute - empty for this command
     },
     [this](bool interrupted) {
-        this->chassis->disableSpeedHelper();
+        resetSpeedHelper();
     },
-    [this]() {
-        return xPIDController.AtGoal() && yPIDController.AtGoal() && headingPIDController.AtGoal();
+        [this]() {
+        return this->alignSpeedHelper->isAtTarget();
     },
-    {chassis}).ToPtr();
-}
-
-void AlignManager::setHeading(Heading heading) {
-    this->heading = heading;
-}
-
-Heading AlignManager::getHeading() {
-    return heading;
-}
-
-void AlignManager::setAlgaePose(AlgaePose algaePose) {
-    this->algaePose = algaePose;
-}
-
-AlgaePose AlignManager::getAlgaePose() {
-    return algaePose;
+        { chassis }).ToPtr();
 }
--- src/main/Subsystems/Arm/ArmConstants.h
+++ src/main/Subsystems/Arm/ArmConstants.h
@@ -10,57 +10,56 @@
 
 struct ArmConstants {
 
-	constexpr static const units::degree_t ArmRangeError = 2.0_deg;
+    constexpr static const units::degree_t ArmRangeError = 2.0_deg;
 
-	inline static const std::map<Positions, units::degree_t> ArmFront{ {Positions::InitialPosition, -90_deg}, {
-			Positions::SustainedPosition, 90.0_deg}, {Positions::Intake, 90.0_deg}, {Positions::IntakeCoralStation,
-			90_deg}, {Positions::AlgaeHighReef, 0.0_deg}, {Positions::AlgaeLowReef, 0.0_deg}, {Positions::AlgaeGround,
-			30.0_deg}, {Positions::L1Position, 90_deg}, {Positions::L1Confirm, 90.0_deg},
-			{Positions::CoralHold, 90.0_deg}, {Positions::CoralAndAlgae, -90.0_deg}, {Positions::AlgaeHold, -90.0_deg},
-			{Positions::L2Front, -54.0_deg}, {Positions::L3Front, 307.0_deg}, {Positions::L4Front, 303.0_deg}, {
-					Positions::L2FrontConfirm, -1.0_deg}, {Positions::L3FrontConfirm, 362.0_deg}, {
-					Positions::L4FrontConfirm, 342.0_deg}, {Positions::NetPosition, -120.0_deg}, {Positions::NetConfirm,
-					-80.0_deg}, {Positions::ProcessorPosition, 0.0_deg}, {Positions::ProcessorConfirm, 0.0_deg}, {
-					Positions::EndPosition, -68.0_deg}, {Positions::L4FrontAuto, -57.0_deg}, {
-					Positions::L4FrontAutoConfirm, -18.0_deg} };
+    inline static const std::map<Positions, units::degree_t> ArmFront { {Positions::InitialPosition, -90_deg}, {
+            Positions::SustainedPosition, 90.0_deg}, {Positions::Intake, 90.0_deg}, {Positions::IntakeCoralStation,
+            90_deg}, {Positions::AlgaeHighReef, 0.0_deg}, {Positions::AlgaeLowReef, 0.0_deg}, {Positions::AlgaeGround,
+            35.0_deg}, {Positions::L1Position, 90_deg}, {Positions::L1Confirm, 90.0_deg}, {Positions::CoralHold,
+            90.0_deg}, {Positions::CoralAndAlgae, -90.0_deg}, {Positions::AlgaeHold, -90.0_deg}, {Positions::L2Front,
+            -54.0_deg}, {Positions::L3Front, 307.0_deg}, {Positions::L4Front, 303.0_deg}, {Positions::L2FrontConfirm,
+            -1.0_deg}, {Positions::L3FrontConfirm, 362.0_deg}, {Positions::L4FrontConfirm, 342.0_deg}, {
+            Positions::NetPosition, -120.0_deg}, {Positions::NetConfirm, -80.0_deg}, {Positions::ProcessorPosition,
+            0.0_deg}, {Positions::ProcessorConfirm, 0.0_deg}, {Positions::EndPosition, -68.0_deg}, {
+            Positions::L4FrontAuto, -57.0_deg}, {Positions::L4FrontAutoConfirm, -18.0_deg}};
 
-	inline static const std::map<Positions, units::degree_t> ArmBack{ {Positions::L2Back, 234.0_deg}, {
-			Positions::L3Back, -127.0_deg}, {Positions::L4Back, -123.0_deg}, {Positions::L2BackConfirm, 179.0_deg}, {
-			Positions::L3BackConfirm, -182.0_deg}, {Positions::L4BackConfirm, -162.0_deg} };
+    inline static const std::map<Positions, units::degree_t> ArmBack { {Positions::L2Back, 234.0_deg}, {
+            Positions::L3Back, -127.0_deg}, {Positions::L4Back, -123.0_deg}, {Positions::L2BackConfirm, 179.0_deg}, {
+            Positions::L3BackConfirm, -182.0_deg}, {Positions::L4BackConfirm, -162.0_deg}};
 
-	constexpr static const units::turns_per_second_t ArmCruiseVelocity = 9_tps;
-	constexpr static const units::turns_per_second_squared_t ArmCruiseAcceleration = 6_tr_per_s_sq;
+    constexpr static const units::turns_per_second_t ArmCruiseVelocity = 9_tps;
+    constexpr static const units::turns_per_second_squared_t ArmCruiseAcceleration = 6_tr_per_s_sq;
 
-	constexpr static const double ArmRotorToSensor = 60;
+    constexpr static const double ArmRotorToSensor = 60;
 
-	constexpr static const double ArmMotorId = 23;
-	constexpr static const double ArmCANCoderId = 25;
+    constexpr static const double ArmMotorId = 23;
+    constexpr static const double ArmCANCoderId = 25;
 
-	constexpr static const OverTalonFXConfig ArmConfig() { //LImites cuestionables
-		OverTalonFXConfig armConfig;
-		armConfig.MotorId = ArmMotorId;
-		armConfig.NeutralMode = ControllerNeutralMode::Brake;
-		armConfig.useFOC = true;
-		armConfig.Inverted = true;
+    constexpr static const OverTalonFXConfig ArmConfig() { //LImites cuestionables
+        OverTalonFXConfig armConfig;
+        armConfig.MotorId = ArmMotorId;
+        armConfig.NeutralMode = ControllerNeutralMode::Brake;
+        armConfig.useFOC = true;
+        armConfig.Inverted = true;
 
-		armConfig.ClosedLoopRampRate = 0.05_s;
-		armConfig.CurrentLimit = 30_A;
-		armConfig.StatorCurrentLimit = 120_A;
-		armConfig.TriggerThreshold = 40_A;
-		armConfig.TriggerThresholdTime = 0.5_s;
-		armConfig.PIDConfigs.GravityType = 1;
-		armConfig.PIDConfigs.WithKG(0.32).WithKV(0).WithKP(170);
+        armConfig.ClosedLoopRampRate = 0.05_s;
+        armConfig.CurrentLimit = 30_A;
+        armConfig.StatorCurrentLimit = 120_A;
+        armConfig.TriggerThreshold = 40_A;
+        armConfig.TriggerThresholdTime = 0.5_s;
+        armConfig.PIDConfigs.GravityType = 1;
+        armConfig.PIDConfigs.WithKG(0.32).WithKV(0).WithKP(170);
 
-		return armConfig;
-	}
+        return armConfig;
+    }
 
-	constexpr static const CanCoderConfig ArmCANConfig() {
-		CanCoderConfig armCANConfig;
-		armCANConfig.CanCoderId = ArmCANCoderId;
-		armCANConfig.Offset = -0.046142578125_tr;
-		armCANConfig.SensorDirection = ctre::phoenix6::signals::SensorDirectionValue::CounterClockwise_Positive;
+    constexpr static const CanCoderConfig ArmCANConfig() {
+        CanCoderConfig armCANConfig;
+        armCANConfig.CanCoderId = ArmCANCoderId;
+        armCANConfig.Offset = -0.046142578125_tr;
+        armCANConfig.SensorDirection = ctre::phoenix6::signals::SensorDirectionValue::CounterClockwise_Positive;
 
-		return armCANConfig;
-	}
+        return armCANConfig;
+    }
 
 };
